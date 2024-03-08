00fd080c7eda791d8c5e1d3a53d377b332a5a078
Your Name <voilaanwi1@gmail.com>
2024-03-08 00:27:42 -0600

    done

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -147,8 +147,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATIC_URL = "/static/"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_TMP = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 os.makedirs(STATIC_TMP, exist_ok=True)
