6c51792bf149623aae8718da22a5691168934e89
Your Name <voilaanwi1@gmail.com>
2024-03-08 00:08:41 -0600

    done

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -155,7 +155,7 @@ os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR /"static"),
+    os.path.join(BASE_DIR ,"static"),
 )
 
 
---
+++ staticfiles/js/post.9c762a5f5a30.js
@@ -0,0 +1,12 @@
+
+
+$(function(){
+    $('.js-menu-icon').click(function(){
+        $(this).next().toggle();
+    })
+
+})
+
+
+
+  
